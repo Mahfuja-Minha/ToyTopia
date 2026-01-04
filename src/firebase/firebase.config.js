d22a87b387504552b9a4066ba19c063b8094db0d
minha <mahfujaakterminha@gmail.com>
2026-01-04 22:03:47 +0600

    env variable added and added .env in gitignore

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCJLaqOkLObnhz56y71QNG-4TaGM0iWL90",
-  authDomain: "toytopia-f27ce.firebaseapp.com",
-  projectId: "toytopia-f27ce",
-  storageBucket: "toytopia-f27ce.firebasestorage.app",
-  messagingSenderId: "769483469114",
-  appId: "1:769483469114:web:5dca000d4cf8f939a4a003"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
